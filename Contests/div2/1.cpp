f3ce9f53f7a464cce4aea1153af064cb28743b79
= <=>
2025-03-30 15:29:18 +0530

    arranging

--- Contests/div2/1.cpp
+++ Contests/div2/1.cpp
@@ -19,33 +19,9 @@ int main()
     }
     for (int i = 0; i < n; i++)
     {
-        cout << arr[i] + 2 << " ";
-    } cout << endl;
+        cout << arr[i] + 10 << " ";
+    }
+    cout << endl;
     cout << "end";
     return 0;
 }
-
-// {
-//     "version": "2.0.0",
-//     "tasks": [
-//         {
-//             "label": "Compile and Run with Input/Output",
-//             "type": "shell",
-//             "command": "powershell.exe",
-//             "args": [
-//                 "-c",
-//                 "g++ -std=c++17 -o ${fileBasenameNoExtension} ${file}; Get-Content input.txt | .\\${fileBasenameNoExtension} | Tee-Object -FilePath output.txt; Get-Content output.txt"
-//             ],
-//             "group": {
-//                 "kind": "build",
-//                 "isDefault": true
-//             },
-//             "presentation": {
-//                 "reveal": "silent",
-//                 "revealProblems": "onProblem",
-//                 "focus": false,
-//                 "panel": "shared"
-//             }
-//         }
-//     ]
-// }
